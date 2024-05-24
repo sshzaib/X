f632c22154e05e91b2409f28c8793505bb4b2305
Zaib-dev <shahzaib01.h@gmail.com>
2024-05-24 16:21:52 +0500

    add scroll on feed

--- clientside/app/page.tsx
+++ clientside/app/page.tsx
@@ -40,43 +40,57 @@ const navbarList: navbarType[] = [
 ];
 export default function Home() {
   return (
-    <div className="grid grid-cols-12 h-screen w-screen px-36 overflow-x-hidden overflow-y-scroll">
-      <div className="col-span-3 static top-0">
-        <div className="cursor-pointer hover:bg-slate-900 w-fit rounded-full p-3 transition-all">
-          <FaXTwitter className="text-3xl" />
-        </div>
-        <ul>
-          {navbarList.map((item) => (
-            <li
-              key={item.title}
-              className="cursor-pointer flex items-center gap-4 text-xl justify-center hover:bg-slate-900 w-fit rounded-full transition-all p-3 pr-7 "
-            >
-              <span className="text-3xl">{item.icon}</span>
-              <span>{item.title}</span>
-            </li>
-          ))}
-        </ul>
-        <div className="mr-14 mt-4">
-          <button className="bg-[#1d9bf0] rounded-full w-full py-3.5 font-bold">
-            Post
-          </button>
-        </div>
-      </div>
-      <div className="col-span-6 border-x border-slate-600">
-        <FeedCard />
-        <FeedCard />
-        <FeedCard />
-        <FeedCard />
-        <FeedCard />
-        <FeedCard />
-        <FeedCard />
-        <FeedCard />
-        <FeedCard />
-        <FeedCard />
-        <FeedCard />
-        <FeedCard />
-      </div>
-      <div className="col-span-3 "></div>
+    <div className="grid grid-cols-12 h-screen w-screen px-36">
+      <Sidebar />
+      <XFeed />
+      <PeopleRecommendation />
     </div>
   );
+}
+
+function Sidebar(): React.ReactNode {
+  return (
+    <div className="col-span-3 static top-0">
+      <div className="cursor-pointer hover:bg-slate-900 w-fit rounded-full p-3 transition-all">
+        <FaXTwitter className="text-3xl" />
+      </div>
+      <ul>
+        {navbarList.map((item) => (
+          <li
+            key={item.title}
+            className="cursor-pointer flex items-center gap-4 text-xl justify-center hover:bg-slate-900 w-fit rounded-full transition-all p-3 pr-7 "
+          >
+            <span className="text-3xl">{item.icon}</span>
+            <span>{item.title}</span>
+          </li>
+        ))}
+      </ul>
+      <div className="mr-14 mt-4">
+        <button className="bg-[#1d9bf0] rounded-full w-full py-3.5 font-bold">
+          Post
+        </button>
+      </div>
+    </div>
+  );
+}
+function XFeed() {
+  return (
+    <div className="col-span-6 border-x flex-1 border-slate-600">
+      <FeedCard />
+      <FeedCard />
+      <FeedCard />
+      <FeedCard />
+      <FeedCard />
+      <FeedCard />
+      <FeedCard />
+      <FeedCard />
+      <FeedCard />
+      <FeedCard />
+      <FeedCard />
+      <FeedCard />
+    </div>
+  );
+}
+function PeopleRecommendation() {
+  return <div className="col-span-3 "></div>;
 }
